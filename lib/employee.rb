3c482d914c5898c3a6b82b7bf9b05619d2be8623
Vhkan <vhkan@yahoo.com>
2024-03-05 09:50:16 -0700

    Create 3 stores

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,4 @@
+#Employee class (model Employee) defined for DB table 'employees' interaction(provided by AR)
 class Employee < ActiveRecord::Base
 
 end
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,4 @@
+#Store class (model Store) defined for DB table 'stores' interaction (provided by AR)
 class Store < ActiveRecord::Base
 
 end
